181fd2cff0926755a09179dc8bbc0e056b88551b
Linnéa Ridderström <liri5181@rackham1.uppmax.uu.se>
2025-05-12 21:05:19 +0200

    added script for running htseq

---
+++ code/run_htseq.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2025-3-3
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 10:00:00
+#SBATCH -J htseq_count
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=linnea.ridderstrom.5181@student.uu.se
+#SBATCH --output=htseq_%x_%j.out
+
+#Load modules
+module load bioinfo-tools
+module load htseq/0.12.4
+
+#Pathways
+ANNOTATED_DIR=/home/liri5181/genome_analysis_project/analyses/dna/04_annotation/prokka_output
+MAPPING_DIR=/home/liri5181/genome_analysis_project/analyses/rna/02_mapping
+COUNTS_DIR=/home/liri5181/genome_analysis_project/analyses/rna/03_read_counting
+mkdir -p "$COUNTS_DIR/location37" "$COUNTS_DIR/location39"
+
+#Create a function: Run HTseq for one location
+run_htseq_counts() {
+  LOCATION=$1
+  echo ">> Starts HTseq for $LOCATION"
+  
+  for BAM in $MAPPING_DIR/$LOCATION/*.bam; do
+    BIN_NAME=$(basename "$BAM" _rna.bam)
+    GFF_FILE=$ANNOTATED_DIR/$BIN_NAME/${BIN_NAME}_clean.gff
+
+    if [[ -f "$GFF_CLEAN" ]]; then
+      echo "HTSeq-count: $BIN_NAME @ $LOCATION"
+      htseq-count -f bam -r pos -s no -i ID "$BAM" "$GFF_CLEAN" > "$COUNTS_DIR/$LOCATION/${BIN_NAME}_counts.txt"
+    else
+      echo "⚠️  Can't find the .gff file for $BIN_NAME – skips it."
+    fi
+  done
+}
+
+#Run for both locations
+run_htseq_counts location37
+run_htseq_counts location39
+
