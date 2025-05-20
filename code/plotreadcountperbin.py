d34bd4b60d41789ddfe952c000d9f592b705c120
Linnéa Ridderström <liri5181@rackham2.uppmax.uu.se>
2025-05-20 16:24:22 +0200

    added script again

--- code/plotreadcountperbin.py
+++ code/plotreadcountperbin.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 
-# === Sökvägar ===
+#Pathways
 
-base_dir = os.path.dirname(__file__)  # mappen där scriptet ligger
+base_dir = os.path.dirname(__file__)  #The map where the script is
 loc37_dir = os.path.join(base_dir, "location37")
 loc39_dir = os.path.join(base_dir, "location39")
 out_dir = os.path.join(base_dir, "expression_plots_raw")
@@ -13,43 +13,41 @@ out_dir = os.path.join(base_dir, "expression_plots_raw")
 os.makedirs(out_dir, exist_ok=True)
 
 
-# Loopa genom alla bin-filer i location37
+#Loop through all bin-files in location37
 for filename in os.listdir(loc37_dir):
 
     if not filename.endswith("_counts_sorted.txt"):
-        continue  # bara filer som slutar på _counts_sorted.txt
+        continue  #Only files that ends with _counts_sorted.txt
     
-    # resten av koden...
 
     bin_name = filename.replace("_counts_sorted.txt", "")
     file37 = os.path.join(loc37_dir, filename)
     file39 = os.path.join(loc39_dir, filename)
 
     if not os.path.exists(file39):
-        print(f" Hoppar över {bin_name} – saknas i location39")
+        print(f"Skips {bin_name} - missing in location39")
         continue
 
-    # Läs in counts
+    #Read counts files and filter out the rows that start with _
     df37 = pd.read_csv(file37, sep="\t", header=None, names=["Gene", "Count_loc37"])
     df37 = df37[~df37["Gene"].str.startswith("_")]
     df39 = pd.read_csv(file39, sep="\t", header=None, names=["Gene", "Count_loc39"])
     df39 = df39[~df39["Gene"].str.startswith("_")]
 
 
-    # Slå ihop på gen-namn
+    #Merge on gene name
     merged = pd.merge(df37, df39, on="Gene", how="outer").fillna(0)
 
-    # Ta de 20 generna med högst counts per location (var för sig)
+    #Choose the top 20 genes with highest read count per location 
     top20_loc37 = merged.nlargest(20, 'Count_loc37')
     top20_loc39 = merged.nlargest(20, 'Count_loc39')
 
-    # Slå ihop dessa två set med gener (för att få alla top gener)
+    #Merge them to get all top genes per bin
     top_genes = pd.concat([top20_loc37, top20_loc39]).drop_duplicates('Gene').reset_index(drop=True)
 
-    # Sortera för snygg plotting (t.ex. på location37 counts)
     top_genes = top_genes.sort_values(by='Count_loc37', ascending=False)
 
-    # Skapa plot med grupperade staplar
+    #Create the plot
     x = np.arange(len(top_genes))
     width = 0.35
 
@@ -65,7 +63,7 @@ for filename in os.listdir(loc37_dir):
     ax.legend()
     plt.tight_layout()
 
-    # Spara fil
+    #Save the files
     plot_file = os.path.join(out_dir, f"{bin_name}_top20_readcounts.png")
     plt.savefig(plot_file)
     plt.close()
